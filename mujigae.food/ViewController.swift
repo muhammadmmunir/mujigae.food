14c8a9918ab0f9d9340611f71e2bad30afb6fa04
Muhammad M. Munir <munir@volantis.io>
2019-07-03 20:40:45 +0700

    Initial Commit

---
+++ mujigae.food/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mujigae.food
+//
+//  Created by Muhammad M. Munir on 03/07/19.
+//  Copyright © 2019 Muhammad M. Munir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
